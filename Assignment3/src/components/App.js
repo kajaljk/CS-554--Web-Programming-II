f92a62ed6b239e4302377ac82ebf92b8a14b2ff2
Kajal Khunt <37006819+kajaljk@users.noreply.github.com>
2018-08-08 11:29:21 -0400

    changes in app file

--- Assignment3/src/components/App.js
+++ Assignment3/src/components/App.js
@@ -34,7 +34,7 @@ class App extends Component {
   
 	  if(!hashParams.access_token) {
       console.log("Token : "+ hashParams.access_token);
-	    window.location.href = 'https://accounts.spotify.com/authorize?client_id=b0bd6051c7c54d8db67e5b62a9a76408&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=http://localhost:3000/callback';
+	    window.location.href = 'https://accounts.spotify.com/authorize?client_id=YOURAPICLIENTID&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=http://localhost:3000/callback';
 	  } else {
       this.setState({access_token: hashParams.access_token});
       console.log("Token : "+ hashParams.access_token);
